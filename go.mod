2e2f66f3c5c437d861a445f6a1e35b6c4799d27f
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-06 13:16:29 +0300

    progress

---
+++ go.mod
@@ -0,0 +1,5 @@
+module baris-inandi/langproject
+
+go 1.17
+
+require github.com/sabhiram/go-gitignore v0.0.0-20210923224102-525f6e181f06 // indirect
--- howmuch.go
+++ howmuch.go
@@ -5,15 +5,19 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	gi "github.com/sabhiram/go-gitignore"
 )
 
 func parseTrackedFile(path string, name string, size int64) {
-  fmt.Println(path)
-  fmt.Println(name)
-  fmt.Println(size)
+  // fmt.Println(path)
+  // fmt.Println(name)
+  // fmt.Println(size)
 }
 
 func main() {
+  x := gi.CompileIgnoreLines(".gitignore")
+  fmt.Println(x)
   err := filepath.Walk(".",
     func(path string, info os.FileInfo, err error) error {
     if err != nil {
