dccc84f4f3286151f49ef68c7dfb08ccc6da886d
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-09 13:16:59 +0300

    fix binary names

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 echo "building binary for windows amd64"
-GOOS=windows GOARCH=amd64 go build -o ./bin/lessonend-win-amd64.exe main.go
+GOOS=windows GOARCH=amd64 go build -o ./bin/lenge-win-amd64.exe main.go
 echo "building binary for darwin amd64"
-GOOS=darwin GOARCH=amd64 go build -o ./bin/lessonend-mac-amd64 main.go
+GOOS=darwin GOARCH=amd64 go build -o ./bin/lenge-mac-amd64 main.go
 echo "building binary for linux amd64"
-GOOS=linux GOARCH=amd64 go build -o ./bin/lessonend-linux-amd64 main.go
+GOOS=linux GOARCH=amd64 go build -o ./bin/lenge-linux-amd64 main.go
 echo "building binary for windows arm"
-GOOS=windows GOARCH=arm go build -o ./bin/lessonend-win-arm.exe main.go
+GOOS=windows GOARCH=arm go build -o ./bin/arm/lenge-win-arm.exe main.go
 echo "building binary for darwin arm64"
-GOOS=darwin GOARCH=arm64 go build -o ./bin/lessonend-mac-arm64 main.go
+GOOS=darwin GOARCH=arm64 go build -o ./bin/arm/lenge-mac-arm64 main.go
 echo "building binary for linux arm"
-GOOS=linux GOARCH=arm go build -o ./bin/lessonend-linux-arm main.go
+GOOS=linux GOARCH=arm go build -o ./bin/arm/lenge-linux-arm main.go
